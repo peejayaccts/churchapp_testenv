28d92a3726b51288c8cd95dcc0fe42f927888bd4
Chan Tiu <b.kristhian.tiu@gmail.com>
2016-07-15 16:51:38 +0800

    >> Change project settings to only include necessary apps >> Removed Session and Admin for the api to be stateless 	modified:   config/settings/base.py

--- ChMS_project/config/settings/base.py
+++ ChMS_project/config/settings/base.py
@@ -31,24 +31,30 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    #    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    #    'django.contrib.sessions',
+    #    'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third party apps
+    'rest_framework',
+    'rest_framework.authtoken',
+    # Internal apps
+    'board'
 ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    #    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
+    #    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    #    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    #    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'config.urls'
 
@@ -76,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'GNT_ChMS_MyDB',
+        'USER': 'pekUrsTruly',
+        'PASSWORD': 'GloriaTai4ndP#k',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
