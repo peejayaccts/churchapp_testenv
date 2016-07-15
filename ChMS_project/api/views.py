6866d2aae257fe1e00f1138184a763e159ec5595
Chan Tiu <b.kristhian.tiu@gmail.com>
2016-07-15 19:30:54 +0800

    >> Add API Search: >>> api/churches/?search=Church >>> api/people/?search=FirstName   	  modified:   api/views.py

--- ChMS_project/api/views.py
+++ ChMS_project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import authentication, permissions, viewsets
+from rest_framework import authentication, permissions, viewsets, filters
 
 from .models import Church, Person
 from .serializers import ChurchSerializer, PersonSerializer
@@ -21,15 +21,20 @@ class DefaultsMixin(object):
     paginate_by = 25
     paginate_by_param = 'page_size'
     max_paginate_by = 100
+    filter_backends = (
+        filters.SearchFilter,
+    )
 
 
 class ChurchViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API Endpoint for listing and creating daughter churches"""
     queryset = Church.objects.all()
     serializer_class = ChurchSerializer
+    search_fields = ('church_name', )
 
 
 class PersonViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API Endpoint for listing and creating daughter churches"""
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    search_fields = ('first_name', 'last_name',)
