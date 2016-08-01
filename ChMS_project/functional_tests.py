0f04599e28675e8618b440d99fdff8b070ebb02c
Chan Tiu <b.kristhian.tiu@gmail.com>
2016-08-01 11:18:42 +0800

    >> Add Functional Test for API:   	new file:   ChMS_project/api_functional_tests.py >> Add Functional Test for UI/API Client 	new file:   ChMS_project/functional_tests.py

---
+++ ChMS_project/functional_tests.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+import unittest
+
+
+class AdminTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_RootPageIsFound(self):
+        self.browser.get('http://127.0.0.1:8000/')
+        self.assertIn('Church Management System', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
