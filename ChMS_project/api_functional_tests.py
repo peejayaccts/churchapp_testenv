48cd377337f93caac56ffe42dab965e148c369e0
Chan Tiu <b.kristhian.tiu@gmail.com>
2016-08-03 17:10:34 +0800

    >> API Interests : Add Functional Test for >>> 1. exceeding 255 characters >>> 2. search >>> 3. sort ascending >>> 4. sort descending 	modified:   api_functional_tests.py

--- ChMS_project/api_functional_tests.py
+++ ChMS_project/api_functional_tests.py
@@ -2,6 +2,12 @@ from selenium import webdriver
 import unittest
 import requests
 import pprint
+import random
+import string
+
+
+def randomword(length):
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
 
 added_test_data = []
 
@@ -18,69 +24,107 @@ class InterestAPITest(unittest.TestCase):
         while added_test_data:
             response = requests.get(
                 self.api['interests'] + str(added_test_data.pop()))
-            self.assertTrue(response.status_code, 200)
+            self.assertTrue(response.status_code == 200)
             interest = response.json()
             #pprint.pprint('Deleting Interest: ' + interest['name'])
             del_response = requests.delete(interest['links']['self'])
-            self.assertTrue(del_response.status_code, 204)
+            self.assertTrue(del_response.status_code == 204)
 
     def test_interest_resource_found(self):
         self.assertTrue('interests' in self.api)
 
-    def test_add_interest(self, interest='Test: Interest 1'):
-        pprint.pprint('Adding interest: ' + interest)
+    def test_add_interest(self, interest=randomword(200)):
+        # pprint.pprint('Adding interest: ' + interest)
         response = requests.post(
             self.api['interests'], data={'name': interest})
-        self.assertTrue(response.status_code, 201)
+        self.assertTrue(response.status_code == 201)
         response_json = response.json()
         # pprint.pprint(response_json)
         added_test_data.append(response_json['id'])
 
     def test_add_duplicate(self):
+        same_name = randomword(200)
+        self.test_add_interest(same_name)
+        # pprint.pprint('Duplicate interest: ' + same_name)
         response = requests.post(self.api['interests'],
-                                 data={'name': 'X'})
-        self.assertTrue(response.status_code, 201)
-        response_json = response.json()
-        added_test_data.append(response_json['id'])
+                                 data={'name': same_name})
+        #self.assertFalse(response.status_code, 201)
+        self.assertTrue(response.status_code == 400)
 
-        response = requests.post(self.api['interests'],
-                                 data={'name': 'X'})
-        self.assertFalse(response.status_code, 201)
-        self.assertTrue(response.status_code, 400)
+    def test_add_256char(self):
+        str256 = randomword(256)
+        response = requests.post(self.api['interests'], data={'name': str256})
+        #self.assertFalse(response.status_code, 201)
+        self.assertTrue(response.status_code == 400)
 
-    def test_get_interest(self):
+    def test_get_all_interest(self):
+        response = requests.get(self.api['interests'])
+        self.assertTrue(response.status_code == 200)
+        # pprint.pprint('Interest List: ')
+        # pprint.pprint(response.json())
+
+    def test_search_interest(self):
+        new_interest = randomword(20)
+        self.test_add_interest(new_interest)
         response = requests.get(
-            self.api['interests'], auth=('demo', 'demodemo'))
-        self.assertTrue(response.status_code, 200)
-        pprint.pprint('Interest List: ')
-        pprint.pprint(response.json())
+            self.api['interests'] + '?search=' + new_interest)
+        self.assertTrue(response.status_code == 200)
+        self.assertTrue(len(response.json()) == 1)
+
+    def test_sort_asc_name(self):
+        new_interest = 'A' * 10
+        self.test_add_interest(new_interest)
+        another_interest = 'Z' * 10
+        self.test_add_interest(another_interest)
+        response = requests.get(self.api['interests'] + '?ordering=name')
+        interests = response.json()
+        latest_interest = ''
+        for interest in interests:
+            if interest['name'] == new_interest or interest['name'] == another_interest:
+                latest_interest = interest['name']
+        self.assertTrue(latest_interest == another_interest,
+                        "Ascending order not ok")
+
+    def test_sort_desc_name(self):
+        new_interest = 'A' * 10
+        self.test_add_interest(new_interest)
+        another_interest = 'Z' * 10
+        self.test_add_interest(another_interest)
+        response = requests.get(self.api['interests'] + '?ordering=-name')
+        interests = response.json()
+        latest_interest = ''
+        for interest in interests:
+            if interest['name'] == new_interest or interest['name'] == another_interest:
+                latest_interest = interest['name']
+        self.assertTrue(latest_interest == new_interest,
+                        "Descending order not working")
 
     def test_update_interest(self):
-        self.test_add_interest('Test: Update Interest')
+        new_interest = randomword(200)
+        self.test_add_interest(new_interest)
         response = requests.get(self.api['interests'])
-        self.assertTrue(response.status_code, 200)
+        self.assertTrue(response.status_code == 200)
         interests = response.json()
         for interest in interests:
-            if interest['name'] == 'Test: Update Interest':
-                pprint.pprint('Updating Interest: ' + interest['name'])
+            if interest['name'] == new_interest:
+                # pprint.pprint('Updating Interest: ' + interest['name'])
                 interest['name'] = 'Test: Updated Interest'
                 put_response = requests.put(interest['links']['self'],
                                             data=interest)
-                self.assertTrue(put_response.status_code, 200)
+                self.assertTrue(put_response.status_code == 200)
                 put_response_json = put_response.json()
-                self.assertTrue(put_response_json['name'],
-                                interest['name'])
-                pprint.pprint('to : ' + put_response_json['name'])
+                self.assertTrue(put_response_json['name'] == interest['name'])
+                # pprint.pprint('to : ' + put_response_json['name'])
 
     def test_delete_interest(self):
-        self.test_add_interest('Test1')
+        self.test_add_interest()
         current_id = added_test_data[-1]
         response = requests.get(self.api['interests'] + str(current_id))
-        self.assertTrue(response.status_code, 200)
+        self.assertTrue(response.status_code == 200)
         interest = response.json()
-        pprint.pprint('Deleting Interest: ' + interest['name'])
+        # pprint.pprint('Deleting Interest: ' + interest['name'])
         del_response = requests.delete(interest['links']['self'])
-        self.assertTrue(del_response.status_code, 204)
+        self.assertTrue(del_response.status_code == 204)
         added_test_data.pop()
 
 if __name__ == '__main__':
