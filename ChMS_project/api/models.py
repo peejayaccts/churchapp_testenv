f67cebc519c28527fb729fc45f587448eaad4384
Chan Tiu <b.kristhian.tiu@gmail.com>
2016-08-03 19:44:50 +0800

    >> Api: Ministries: Database 	new file:   api/migrations/0007_ministry.py 	modified:   api/models.py

--- ChMS_project/api/models.py
+++ ChMS_project/api/models.py
@@ -79,3 +79,10 @@ class SpiritualMilestone(models.Model):
 
     def __str__(self):
         return (self.name.title())
+
+
+class Ministry(models.Model):
+    name = models.CharField(max_length=255, unique=True, blank=False)
+
+    def __str__(self):
+        return (self.name.title())
