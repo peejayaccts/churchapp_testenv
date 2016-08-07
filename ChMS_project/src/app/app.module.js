40a9f00938eefb4d45810d6c7cd9e0642f5f7f32
ghost-user <ghost-user@gmail.com>
2016-08-07 16:19:22 +0800

    >>UI: Restructured Angular folder to best practices

---
+++ ChMS_project/src/app/app.module.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name appsApp
+ * @description
+ * # appsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('appsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ ChMS_project/src/app/components/ministries/ministriesController.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appsApp.components.ministries:MinistriesCtrl
+ * @description
+ * # MinistriesCtrl
+ * Controller of the appsApp
+ */
+angular.module('appsApp')
+  .controller('MinistriesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
