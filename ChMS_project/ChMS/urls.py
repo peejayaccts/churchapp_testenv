f37a8ffd2aeb1e5aeef3d053bbeab83f2c4013b1
vuvuzella <j.tabacii@gmail.com>
2016-08-07 19:12:28 +0800

    Put the url for the ui inside ChMS for it to be self-contained.

--- ChMS_project/ChMS/urls.py
+++ ChMS_project/ChMS/urls.py
@@ -16,12 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
-from rest_framework.authtoken.views import obtain_auth_token
-
-from api.urls import router
 
 urlpatterns = [
-    url(r'^api/token/', obtain_auth_token, name="api-token"),
-    url(r'^api/', include(router.urls)),
-    url(r'^', include('ChMS.urls')),
+    url(r'^', TemplateView.as_view(template_name='ChMS/index.html')),
 ]
