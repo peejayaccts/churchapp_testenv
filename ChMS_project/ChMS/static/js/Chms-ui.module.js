e9004d7a15f4c4d93135326e5c43637dd1dcfbfa
vuvuzella <j.tabacii@gmail.com>
2016-08-08 10:31:41 +0800

    Initial structure of church page until I get internet and take good look at specs again

--- ChMS_project/ChMS/static/js/Chms-ui.module.js
+++ ChMS_project/ChMS/static/js/Chms-ui.module.js
@@ -1,12 +1,19 @@
 var chmsUi = angular.module('Chms-ui', []);
 
-chmsUi.controller('IndexController', ['$scope',
-		function($scope) {
+chmsUi.controller('IndexController', ['$scope', '$http',
+		function($scope, $http) {
 			$scope.names = [
 				{ name: 'Jon' },
 				{ name: 'Pj' },
 				{ name: 'Pao' },
 				{ name: 'Chan' },
 			];
+			$scope.churches = [];
+			$http.get('http://localhost:8000/api/churches/')
+				.then(function(result) {
+					// angular.forEach(result.data) {
+					// 	// $scope.churches.push()
+					// };
+				});
 		}
 ]);
--- ChMS_project/ChMS/urls.py
+++ ChMS_project/ChMS/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.views.generic import TemplateView
 
+# from . import views
+
+app_name = 'ChMS'
 
 urlpatterns = [
-    url(r'^', TemplateView.as_view(template_name='ChMS/index.html')),
+    url(r'^$', TemplateView.as_view(template_name='ChMS/index.html')),
+    url(r'^church/', TemplateView.as_view(template_name='ChMS/church.html'), name='church'),
 ]
