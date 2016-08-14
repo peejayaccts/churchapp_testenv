3b13254923f4a8ee246beb99a339a7ed6726a958
ghost-user <ghost-user@gmail.com>
2016-08-14 14:32:34 +0800

    SysAdmin: Update Script 2

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ev
 
 cd ChMS_project/
-python manage.py migrate --noinput
+python manage.py migrate 
 python manage.py runserver & \
     python api_functional_tests.py & \
     python functional_tests.py
