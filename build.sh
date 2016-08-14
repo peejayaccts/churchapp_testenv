0efa81aaeaf8b53cb8ce812884533eacc89e055e
ghost-user <ghost-user@gmail.com>
2016-08-14 14:43:41 +0800

    >>SysAdmin: Fix script

--- build.sh
+++ build.sh
@@ -4,6 +4,5 @@ set -ev
 
 cd ChMS_project/
 python manage.py migrate 
-python manage.py runserver & \
-    python api_functional_tests.py & \
-    python functional_tests.py
+python manage.py runserver 
+python api_functional_tests.py & python functional_tests.py
