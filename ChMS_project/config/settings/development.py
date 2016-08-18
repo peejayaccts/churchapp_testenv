dbc9d25d0eb1f60116654c66f366bb1f88887717
peejayaccts <peejayaccts@gmail.com>
2016-08-18 17:33:06 +0800

    >>SysAdmin: Set development URL

---
+++ ChMS_project/config/settings/development.py
@@ -0,0 +1,18 @@
+# settings/production.py
+from .base import *
+
+DEBUG = False
+COMPRESS_ENABLED = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'GNT_ChMS_MyDB',
+        'USER': 'pekUrsTruly',
+        'PASSWORD': 'GloriaTai4ndP#k',
+        'HOST':'ec2-54-152-188-2.compute-1.amazonaws.com',
+        'PORT': '3306',
+    }
+}
+
+#INSTALLED_APPS += ("debug_toolbar", )
