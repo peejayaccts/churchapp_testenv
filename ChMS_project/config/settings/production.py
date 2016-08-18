59f316efed21aa698c644630ff1b332e5a023786
peejayaccts <peejayaccts@gmail.com>
2016-08-18 19:31:49 +0800

    >>SysAdmin: Add Prod config

--- ChMS_project/config/settings/production.py
+++ ChMS_project/config/settings/production.py
@@ -10,7 +10,8 @@ DATABASES = {
         'NAME': 'GNT_ChMS_MyDB',
         'USER': 'pekUrsTruly',
         'PASSWORD': 'GloriaTai4ndP#k',
-        'HOST': '127.0.0.1',
+        #'HOST': '127.0.0.1',
+        'HOST': 'ec2-54-152-188-2.compute-1.amazonaws.com' ,
         'PORT': '3306',
     }
 }
