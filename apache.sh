2830e939ed6739fb2cffd52c586a37c089a623f0
peejayaccts <peejayaccts@gmail.com>
2016-08-22 22:28:31 +0800

    >>SysAdmin: Travis apache 6

--- apache.sh
+++ apache.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -e
 
 sudo a2enmod ssl
+sudo service apache2 restart
+
+sudo mkdir /etc/apache2/ssl/
+#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#                -keyout /etc/apache2/ssl/apache.key \
+#                -out /etc/apache2/ssl/apache.crt
 
 sudo cp /etc/apache2/sites-available/default-ssl.conf \
         /etc/apache2/sites-available/default-ssl.conf.bak 
