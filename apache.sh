2f79555ab89fcc5d6df4e38a707e34362487dbea
peejayaccts <peejayaccts@gmail.com>
2016-08-22 22:54:34 +0800

    >>SysAdmin: Travis apache 8

--- apache.sh
+++ apache.sh
@@ -3,15 +3,15 @@
 sudo a2enmod ssl
 sudo service apache2 restart
 
-sudo mkdir /etc/apache2/ssl/
-#sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+# sudo mkdir /etc/apache2/ssl/
+# sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 #                -keyout /etc/apache2/ssl/apache.key \
 #                -out /etc/apache2/ssl/apache.crt
 
 sudo cp /etc/apache2/sites-available/default-ssl.conf \
         /etc/apache2/sites-available/default-ssl.conf.bak 
 
-cat chms-default-ssl.conf > etc/apache2/sites-available/default-ssl.conf 
+cat chms-default-ssl.conf > /etc/apache2/sites-available/default-ssl.conf 
 
 sudo cat /etc/apache2/sites-available/default-ssl.conf
 
